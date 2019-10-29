5645e5b7847f5bad2b7f70ad1159c4d63ac825f4
LaurentMox <LaurentMox@users.noreply.github.com>
2019-10-29 10:48:50 +0100

    Format config

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,21 @@ import postcss from "rollup-plugin-postcss";
 import svgr from "@svgr/rollup";
 
 export default {
-  input: "Candidates/Widget/index.js",
-  output: {
-    file: "Transformed/Widget.mdr",
-    format: "amd"
-  },
-  plugins: [
-    postcss({
-      extensions: [".css"]
-    }),
-    babel({
-      ...JSON.parse(require("fs").readFileSync(".babelrc", "utf8")),
-      exclude: "node_modules/**",
-      presets: ["@babel/env", "@babel/preset-react"]
-    }),
-    url(),
-    svgr()
-  ]
+    input: "Candidates/Widget/index.js",
+    output: {
+        file: "Transformed/Widget.mdr",
+        format: "amd"
+    },
+    plugins: [
+        postcss({
+            extensions: [".css"]
+        }),
+        babel({
+            ...JSON.parse(require("fs").readFileSync(".babelrc", "utf8")),
+            exclude: "node_modules/**",
+            presets: ["@babel/env", "@babel/preset-react"]
+        }),
+        url(),
+        svgr()
+    ]
 };
