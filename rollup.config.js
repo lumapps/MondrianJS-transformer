ac157bb57bb81676248f6734e6af15c630e0f561
Grégory CAT <gregory.cat.gc@gmail.com>
2019-10-29 11:03:49 +0100

    add import url in config

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,24 @@
 import babel from "rollup-plugin-babel";
 import postcss from "rollup-plugin-postcss";
 import svgr from "@svgr/rollup";
+import url from "rollup-plugin-url";
 
 export default {
-    input: "Candidates/Widget/index.js",
-    output: {
-        file: "Transformed/Widget.mdr",
-        format: "amd"
-    },
-    plugins: [
-        postcss({
-            extensions: [".css"]
-        }),
-        babel({
-            ...JSON.parse(require("fs").readFileSync(".babelrc", "utf8")),
-            exclude: "node_modules/**",
-            presets: ["@babel/env", "@babel/preset-react"]
-        }),
-        url(),
-        svgr()
-    ]
+  input: "Candidates/App.js",
+  output: {
+    file: "Transformed/Widget.mdr",
+    format: "amd"
+  },
+  plugins: [
+    postcss({
+      extensions: [".css"]
+    }),
+    babel({
+      ...JSON.parse(require("fs").readFileSync(".babelrc", "utf8")),
+      exclude: "node_modules/**",
+      presets: ["@babel/env", "@babel/preset-react"]
+    }),
+    url(),
+    svgr()
+  ]
 };
